856ba2218ac05ab161ef0ebc6e5ead450fe4837a
CMMhero <ch.mh.marvelio@gmail.com>
2023-12-03 19:28:57 +0700

    simple responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,25 @@ function App() {
 	}, []);
 
 	return (
-		<div className="App">
-			{games &&
-				games.results.map((game) => (
-					<div key={game.id}>{game.name}</div>
-				))}
+		<div className="container flex justify-center p-16">
+			<div className="grid grid-flow-row grid-cols-2 gap-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4">
+				{games &&
+					games.results.map((game) => (
+						<div className="w-auto" key={game.id}>
+							<img
+								className="aspect-video rounded-2xl object-fit-contain"
+								src={game.background_image}
+							/>
+							<h2 className="text-xl text-red-400">{game.name}</h2> {game.rating}/{game.rating_top}
+							{game.parent_platforms.map((platform) => (
+								<p key={platform.platform.id}>{platform.platform.name}</p>
+							))}
+							{game.genres.map((genre) => (
+								<p key={genre.id}>{genre.name}</p>
+							))}
+						</div>
+					))}
+			</div>
 		</div>
 	);
 }
