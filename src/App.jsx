dfc142d5eee13bd35240956a9ae2e5e831badb74
CMMhero <ch.mh.marvelio@gmail.com>
2023-12-03 16:04:50 +0700

    data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const baseUrl = "https://api.rawg.io/api/";
+	const [games, setGames] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		fetch(baseUrl + "games?key=dc6f3f19206d43078b51b87ab10705b1")
+			.then((response) => response.json())
+			.then((data) => setGames(data))
+			.catch((error) => console.log(error));
+
+		console.log(games);
+	}, []);
+
+	return (
+		<>
+			<div className="w-full h-100 text-4xl flex justify-center text-blue-400">
+				Nice
+				{games &&
+					games.results.map((game) => {
+						<div key={game.id}>{game.name}</div>;
+					})}
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
