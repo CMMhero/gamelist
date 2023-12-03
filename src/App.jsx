dee06a3448c8bad02f1ab79f8c8841a9a0861ab1
CMMhero <ch.mh.marvelio@gmail.com>
2023-12-03 18:45:55 +0700

    fetch api works

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import { useEffect, useState } from "react";
 
 function App() {
-	const baseUrl = "https://api.rawg.io/api/";
-	const [games, setGames] = useState(null);
+	let [games, setGames] = useState(null);
 
 	useEffect(() => {
-		fetch(baseUrl + "games?key=dc6f3f19206d43078b51b87ab10705b1")
+		fetch("https://api.rawg.io/api/games?key=dc6f3f19206d43078b51b87ab10705b1")
 			.then((response) => response.json())
-			.then((data) => setGames(data))
-			.catch((error) => console.log(error));
-
-		console.log(games);
+			.then((data) => setGames(data));
 	}, []);
 
 	return (
-		<>
-			<div className="w-full h-100 text-4xl flex justify-center text-blue-400">
-				Nice
-				{games &&
-					games.results.map((game) => {
-						<div key={game.id}>{game.name}</div>;
-					})}
-			</div>
-		</>
+		<div className="App">
+			{games &&
+				games.results.map((game) => (
+					<div key={game.id}>{game.name}</div>
+				))}
+		</div>
 	);
 }
 
