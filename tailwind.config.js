f82bb1734d5d68af48826a0641e1aea9a548ee85
CMMhero <ch.mh.marvelio@gmail.com>
2023-12-08 18:35:21 +0700

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        'sans': ['Inter', 'Arial', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
