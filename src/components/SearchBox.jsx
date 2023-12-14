aaff0bb298f0cf56c626d861a94fc27e5c0348e3
CMMhero <ch.mh.marvelio@gmail.com>
2023-12-14 21:24:18 +0700

    search works

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,12 +3,14 @@ import {
 	CommandDialog,
 	CommandEmpty,
 	CommandGroup,
-	CommandInput,
 	CommandItem,
 	CommandList,
 } from "@/components/ui/command";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Image from "./Image";
 
 export default function SearchBox() {
 	const [open, setOpen] = useState(false);
@@ -31,9 +33,10 @@ export default function SearchBox() {
 		const fetchData = async () => {
 			try {
 				const response = await fetch(
-					`https://api.rawg.io/api/games?key=dc6f3f19206d43078b51b87ab10705b1&search=${searchQuery}&page_size=200`
+					`https://api.rawg.io/api/games?key=dc6f3f19206d43078b51b87ab10705b1&search=${searchQuery}&page_size=10`
 				);
 				const data = await response.json();
+				// console.log(data.results);
 				setSearchResults(data.results || []);
 			} catch (error) {
 				console.error("Error fetching data: ", error);
@@ -65,11 +68,18 @@ export default function SearchBox() {
 			</div>
 
 			<CommandDialog className="rounded-lg" open={open} onOpenChange={setOpen}>
-				<CommandInput
-					placeholder="Search games"
-					// value={searchQuery}
-					onChange={(e) => setSearchQuery(e.target.value)}
-				/>
+				<div className="flex items-center border-b px-3">
+					<Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+					<Input
+						placeholder="Search games"
+						className={
+							"flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 border-none focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+						}
+						onChange={(e) => {
+							setSearchQuery(e.target.value);
+						}}
+					/>
+				</div>
 				<CommandList>
 					<CommandEmpty>No results found.</CommandEmpty>
 					<CommandGroup heading="Results">
@@ -81,7 +91,22 @@ export default function SearchBox() {
 										setOpen(false);
 									}}
 								>
-									{result.name}
+									<div className="flex gap-2 items-center">
+										<div>
+											{result.background_image ? (
+												<Image
+													src={result.background_image}
+													alt={result.name}
+													className="aspect-[1.5/1] object-cover w-20 rounded"
+												/>
+											) : (
+												<div className="aspect-[1.5/1] object-cover w-20 rounded bg-secondary flex items-center text-center justify-center overflow-hidden">
+													<span className="text-xs">{result.name}</span>
+												</div>
+											)}
+										</div>
+										{result.name}
+									</div>
 								</CommandItem>
 							</Link>
 						))}
